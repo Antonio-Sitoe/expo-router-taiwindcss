25a11a3177ef5834ebf150e37d2d7eb6a14b4f5e
Antonio Sitoe <antonio.sitoe@void.co.mz>
2023-06-27 20:54:13 +0200

    add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,80 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Pressable, View, Text, Alert, Button } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from "expo-av";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
-  const [recording, setRecording] = useState<Audio.Recording | null>(null);
-  const [recordingFileUri, setRecordingFileUri] = useState<string | null>(null);
-
-  async function handleRecordingStart() {
-    const { granted } = await Audio.getPermissionsAsync();
-
-    if (granted) {
-      try {
-        const { recording } = await Audio.Recording.createAsync();
-        setRecording(recording);
-      } catch (error) {
-        console.log(error);
-        Alert.alert("Erro ao gravar", "Nao foi possivel gravar");
-      }
-    }
-  }
-
-  async function hadleRecordStop() {
-    try {
-      if (recording) {
-        await recording.stopAndUnloadAsync();
-        const fileUri = recording.getURI();
-        setRecordingFileUri(fileUri);
-        setRecording(null);
-        console.log("FILE URI", fileUri);
-      }
-    } catch (error) {
-      console.log(error);
-      Alert.alert("Erro ao parar a gravacao");
-    }
-  }
-  async function handleAudioPlay() {
-    if (recordingFileUri) {
-      const { sound } = await Audio.Sound.createAsync(
-        { uri: recordingFileUri },
-        { shouldPlay: true }
-      );
-      await sound.setPositionAsync(0);
-      await sound.playAsync();
-    }
-  }
-  useEffect(() => {
-    Audio.requestPermissionsAsync().then(({ granted }) => {
-      if (granted) {
-        Audio.setAudioModeAsync({
-          allowsRecordingIOS: true,
-          interruptionModeIOS: InterruptionModeIOS.DoNotMix,
-          interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
-          shouldDuckAndroid: true,
-          playThroughEarpieceAndroid: true,
-        });
-      }
-    });
-  }, []);
   return (
-    <View className="flex-1 items-center justify-center">
-      <Pressable
-        onPressIn={handleRecordingStart}
-        onPressOut={hadleRecordStop}
-        className={`w-[94] h-[94] rounded-full ${
-          recording ? "bg-lime-400" : "bg-[#d6d6d6]"
-        } items-center justify-center`}
-      >
-        <MaterialIcons name="mic" size={44} color="#252525" />
-      </Pressable>
-      {recording && (
-        <Text className="font-bold text-lg mt-5 mb-5">Gravando...</Text>
-      )}
-      {recordingFileUri && (
-        <Button onPress={handleAudioPlay} title="Ouvir o audio" />
-      )}
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.subtitle}> is the first page of your app.</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
